bf9a40db2988b963a7ef36b39eef6aebb4937002
danil_lans <danimen0001@gmail.com>
2022-02-06 20:27:06 +0300

    auth сервис

---
+++ auth/src/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { connectDb } = require('./helpers/db');
+const { host, port, db, apiUrl } = require('./configuration');
+const axios = require('axios');
+
+const app = express();
+
+const startServer = () => {
+  app.listen(port, () => {
+    console.log(`Started auth service on port: ${port}`);
+    console.log(`On host: ${host}`);
+    console.log(`Our database: ${db}`);
+  });
+};
+
+app.get('/test', (req, res) => {
+  res.send('Our auth server is working correctly');
+});
+
+app.get('/testwithapidata', (req, res) => {
+  axios.get(apiUrl + '/testapidata').then((response) => {
+    response.json({
+      testApiData: response.data.testWithApi,
+    });
+  });
+});
+
+app.get('/api/currentUser', (req, res) => {
+  res.json({
+    id: '1234',
+    email: 'foo@gmail.com',
+  });
+});
+
+connectDb()
+  .on('error', console.log)
+  .on('disconnected', connectDb)
+  .once('open', startServer);
